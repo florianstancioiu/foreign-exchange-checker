1fac62edb33f8673d4cea73fabf2d7d2a3ce3ed5
Florian Stancioiu <florinvictorstancioiu@gmail.com>
2026-06-16 15:40:11 +0300

    Worked on Favorites page

---
+++ src/components/UI/FavoriteItem/FavoriteItem.tsx
@@ -0,0 +1,56 @@
+import ArrowRightSvg from "../../../images/icon-arrow-right.svg?react";
+import StarSvg from "../../../images/icon-star.svg?react";
+import StarFilledSvg from "../../../images/icon-star-filled.svg?react";
+import ChevronDownSvg from "../../../images/icon-chevron-down.svg?react";
+
+export type FavoriteItemProps = {
+  firstCurrency: string;
+  secondCurrency: string;
+  value: number;
+  percentageValue: number;
+  isFavorite?: boolean;
+};
+
+const FavoriteItem = ({
+  firstCurrency,
+  secondCurrency,
+  value,
+  percentageValue,
+  isFavorite,
+}: FavoriteItemProps) => {
+  return (
+    <section className="border border-neutral-500 rounded-[10px] bg-neutral-600 p-3 flex gap-x-2.5 justify-between items-center">
+      <div className="flex items-center gap-2.5 text-sm font-normal leading-[120%] tracking-[1px] text-neutral-50 uppercase">
+        <span>{firstCurrency}</span>
+        <ArrowRightSvg />
+        <span>{secondCurrency}</span>
+      </div>
+      <div className="flex justify-between items-center gap-x-2.5">
+        <div className="text-right">
+          <p className="text-base font-normal leading-[120%] tracking-[1px] text-neutral-50 mb-1.5">
+            {value}
+          </p>
+          <p
+            className={`${percentageValue > 0 ? "text-green-500" : "text-red-500"} text-[10px] font-normal leading-[100%] flex gap-x-1.5 items-center`}
+          >
+            <ChevronDownSvg
+              className={`${percentageValue > 0 ? "rotate-180" : ""} size-4`}
+            />
+            <span>{percentageValue}</span>
+          </p>
+        </div>
+        {isFavorite ? (
+          <div className="size-8 border border-lime-500 text-lime-500 rounded-[10px] grid place-content-center cursor-pointer">
+            <StarFilledSvg />
+          </div>
+        ) : (
+          <div className="size-8 border border-neutral-500 rounded-[10px] grid place-content-center cursor-pointer">
+            <StarSvg />
+          </div>
+        )}
+      </div>
+    </section>
+  );
+};
+
+export default FavoriteItem;
--- src/pages/Favorites.tsx
+++ src/pages/Favorites.tsx
@@ -1,9 +1,116 @@
 import TabsMenu from "../components/TabsMenu/TabsMenu";
+import DetailsContainer from "../components/UI/DetailsContainer/DetailsContainer";
+import FavoriteItem from "../components/UI/FavoriteItem/FavoriteItem";
+
+const headerContent = (
+  <div className="flex justify-between items-start mb-4">
+    <p className="uppercase text-sm font-normal leading-[120%] tracking-[1px] text-neutral-200 mb-2.5">
+      Pinned pairs
+    </p>
+    <p className="text-base font-medium leading-[120%] tracking-[1px] text-neutral-50">
+      10 favorites
+    </p>
+  </div>
+);
+
+const compareItems = [
+  {
+    id: 1,
+    firstCurrency: "usd",
+    secondCurrency: "eur",
+    value: 0.853,
+    percentageValue: -0.16,
+    isFavorite: true,
+  },
+  {
+    id: 2,
+    firstCurrency: "gbp",
+    secondCurrency: "eur",
+    value: 0.853,
+    percentageValue: 0.16,
+    isFavorite: true,
+  },
+  {
+    id: 3,
+    firstCurrency: "usd",
+    secondCurrency: "jpy",
+    value: 0.853,
+    percentageValue: 0.16,
+    isFavorite: true,
+  },
+  {
+    id: 4,
+    firstCurrency: "usd",
+    secondCurrency: "bdt",
+    value: 0.853,
+    percentageValue: 0.16,
+    isFavorite: true,
+  },
+  {
+    id: 5,
+    firstCurrency: "euro",
+    secondCurrency: "gbd",
+    value: 0.853,
+    percentageValue: 0.16,
+    isFavorite: true,
+  },
+  {
+    id: 6,
+    firstCurrency: "aud",
+    secondCurrency: "nzd",
+    value: 0.853,
+    percentageValue: 0.16,
+    isFavorite: true,
+  },
+  {
+    id: 7,
+    firstCurrency: "gbp",
+    secondCurrency: "eur",
+    value: 0.853,
+    percentageValue: 0.16,
+    isFavorite: true,
+  },
+  {
+    id: 8,
+    firstCurrency: "gbp",
+    secondCurrency: "eur",
+    value: 0.853,
+    percentageValue: 0.16,
+    isFavorite: true,
+  },
+  {
+    id: 9,
+    firstCurrency: "gbp",
+    secondCurrency: "eur",
+    value: 0.853,
+    percentageValue: 0.16,
+    isFavorite: true,
+  },
+  {
+    id: 10,
+    firstCurrency: "gbp",
+    secondCurrency: "eur",
+    value: 0.853,
+    percentageValue: 0.16,
+    isFavorite: true,
+  },
+];
 
 const Favorites = () => {
   return (
     <TabsMenu variant="favorites">
-      <h1>Favorites</h1>
+      <DetailsContainer headerContent={headerContent}>
+        {compareItems.map((item) => (
+          <FavoriteItem
+            key={item.id}
+            firstCurrency={item.firstCurrency}
+            secondCurrency={item.secondCurrency}
+            value={item.value}
+            percentageValue={item.percentageValue}
+            isFavorite={item.isFavorite}
+          />
+        ))}
+      </DetailsContainer>
     </TabsMenu>
   );
 };
