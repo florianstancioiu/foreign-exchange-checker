8700da1a14e8d0b7787ecd2b1a22daf99d6acf8c
Florian Stancioiu <florinvictorstancioiu@gmail.com>
2026-06-19 16:43:46 +0300

    Made the Dropdown work with popover and anchor

--- src/components/UI/Dropdown/Dropdown.tsx
+++ src/components/UI/Dropdown/Dropdown.tsx
@@ -1,4 +1,6 @@
 import ChevronDownSvg from "../../../images/icon-chevron-down.svg?react";
+import { Link } from "react-router";
+import { useId } from "react";
 
 export type DropdownValue = {
   id: number;
@@ -14,13 +16,15 @@ export type DropdownProps = {
 };
 
 const Dropdown = ({ values, className }: DropdownProps) => {
+  const dropdownContentId = useId();
   const activeValue = values.find((val) => val.isActive) || values[0];
 
   return (
     <div className={className !== undefined ? className : ""}>
       <button
         type="button"
-        className="px-3 rounded-lg border border-neutral-400 bg-neutral-700 flex justify-between items-center h-10 cursor-pointer w-full"
+        popoverTarget={dropdownContentId}
+        className="dropdown-anchor px-3 rounded-lg border border-neutral-400 bg-neutral-700 flex justify-between items-center h-10 cursor-pointer w-full"
       >
         <p className="text-base font-normal leading-[120%] tracking-[1px] flex items-center gap-x-2">
           <span className="uppercase">{activeValue.title} </span>
@@ -34,9 +38,28 @@ const Dropdown = ({ values, className }: DropdownProps) => {
         </p>
         <ChevronDownSvg />
       </button>
-      <div className="hidden">
+      <div
+        popover=""
+        id={dropdownContentId}
+        className="dropdown-content absolute bg-neutral-700 rounded-[10px] p-2"
+      >
         {values.map((val) => (
-          <div key={val.id}>{val.title}</div>
+          <Link
+            className="w-full p-2.5 flex justify-between"
+            key={val.id}
+            to={val.url}
+          >
+            <span className="text-base font-normal leading-[120% text-neutral-50 tracking-[1px] uppercase">
+              {val.title}
+            </span>
+            {val.suffix !== undefined ? (
+              <span className="rounded-full size-5 inline-grid place-content-center bg-lime-800 text-lime-500 text-[10px] font-normal leading-[100%]">
+                {val.suffix}
+              </span>
+            ) : (
+              <></>
+            )}
+          </Link>
         ))}
       </div>
     </div>
