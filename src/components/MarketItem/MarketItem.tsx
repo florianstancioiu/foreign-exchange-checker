4cf917e81b45b98c95b1162e555b7fd6ee5a16c7
Florian Stancioiu <florinvictorstancioiu@gmail.com>
2026-06-23 14:04:32 +0300

    Disabled text selection for MarketItem

--- src/components/MarketItem/MarketItem.tsx
+++ src/components/MarketItem/MarketItem.tsx
@@ -16,7 +16,7 @@ const MarketItem = ({
   rateDiffPercentage,
 }: MarketItemProps) => {
   return (
-    <div className="flex items-center gap-x-2 px-5 py-2 bg-neutral-700 border-r border-neutral-500 md:py-3">
+    <div className="flex items-center gap-x-2 px-5 py-2 bg-neutral-700 border-r border-neutral-500 md:py-3 select-none">
       <p className="text-neutral-200">
         {baseCurrency}/{quoteCurrency}
       </p>
