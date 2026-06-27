77a611286e23d8640ccf45fe395f7b10b5b23aec
Florian Stancioiu <florinvictorstancioiu@gmail.com>
2026-06-27 11:20:52 +0300

    Created a LogsContext file to deal with logs

---
+++ src/contexts/LogsContext.tsx
@@ -0,0 +1,126 @@
+import { use, createContext, type ReactNode } from "react";
+import { useLocalStorage } from "usehooks-ts";
+import { getTodaysStringDate } from "../helpers/dates";
+
+type LogItem = {
+  id: string;
+  firstCurrency: number;
+  secondCurrency: number;
+  date: Date;
+  base: string;
+  quote: string;
+};
+
+export type LogsState = {
+  logs: LogItem[] | undefined;
+  toggleLog: (
+    base: string,
+    quote: string,
+    firstCurrency: number,
+    secondCurrency: number,
+  ) => void;
+  isLogged: (id: string) => boolean;
+  getLogId: (
+    base: string,
+    quote: string,
+    sendValue: number,
+    receiveValue: number,
+  ) => string;
+  clearAll: () => void;
+};
+
+const LogsContext = createContext<LogsState | null>(null);
+
+export type LogsContextProps = {
+  children: ReactNode;
+};
+
+export const LogsContextProvider = ({ children }: LogsContextProps) => {
+  const [logs, setLogs] = useLocalStorage<LogItem[] | undefined>("logs:v1", []);
+
+  const getLogId = (
+    base: string,
+    quote: string,
+    sendValue: number,
+    receiveValue: number,
+  ) => {
+    return `${base.toUpperCase()}-${quote.toUpperCase()}-${getTodaysStringDate()}-${sendValue}-${receiveValue}`;
+  };
+
+  const toggleLog = (
+    base: string,
+    quote: string,
+    firstCurrency: number,
+    secondCurrency: number,
+  ) => {
+    const id = getLogId(base, quote, firstCurrency, secondCurrency);
+
+    setLogs((logItems) => {
+      if (logItems?.find((item) => item.id === id)) {
+        return logItems.filter((itm) => itm.id !== id);
+      }
+
+      if (logItems === undefined) {
+        return [
+          {
+            id,
+            firstCurrency,
+            secondCurrency,
+            date: new Date(),
+            base: base.toUpperCase(),
+            quote: quote.toUpperCase(),
+          },
+        ];
+      }
+
+      logItems?.push({
+        id,
+        firstCurrency,
+        secondCurrency,
+        date: new Date(),
+        base: base.toUpperCase(),
+        quote: quote.toUpperCase(),
+      });
+
+      return logItems;
+    });
+  };
+
+  const clearAll = () => {
+    setLogs([]);
+  };
+
+  const isLogged = (id: string) => {
+    if (logs === undefined) {
+      return false;
+    }
+
+    return logs.findIndex((item) => item.id === id) >= 0;
+  };
+
+  return (
+    <LogsContext.Provider
+      value={{
+        logs,
+        toggleLog,
+        isLogged,
+        clearAll,
+        getLogId,
+      }}
+    >
+      {children}
+    </LogsContext.Provider>
+  );
+};
+
+export const useLogsContext = () => {
+  const context = use(LogsContext);
+
+  if (!context) {
+    throw new Error(
+      "useLogsContext must be used within <LogsContextProvider />",
+    );
+  }
+
+  return context;
+};
--- src/main.tsx
+++ src/main.tsx
@@ -6,19 +6,22 @@ import Routes from "./routes";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { RateContextProvider } from "./contexts/RateContext";
 import { FavoritesContextProvider } from "./contexts/FavoritesContext";
+import { LogsContextProvider } from "./contexts/LogsContext";
 
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-      <FavoritesContextProvider>
-        <RateContextProvider>
-          <HashRouter>
-            <Routes />
-          </HashRouter>
-        </RateContextProvider>
-      </FavoritesContextProvider>
+      <LogsContextProvider>
+        <FavoritesContextProvider>
+          <RateContextProvider>
+            <HashRouter>
+              <Routes />
+            </HashRouter>
+          </RateContextProvider>
+        </FavoritesContextProvider>
+      </LogsContextProvider>
     </QueryClientProvider>
   </StrictMode>,
 );
