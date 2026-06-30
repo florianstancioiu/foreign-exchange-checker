d593645adcc9203ad778f2687bed9ff14df784f6
Florian Stancioiu <florinvictorstancioiu@gmail.com>
2026-06-30 14:22:22 +0300

    Wrote tests for BUtton and CompareItem

--- src/components/UI/Button/Button.test.tsx
+++ src/components/UI/Button/Button.test.tsx
@@ -2,15 +2,20 @@ import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import Button from "./Button";
 
-test("loads and displays greeting", async () => {
-  // ARRANGE
+test("component render", async () => {
   render(<Button>Hello World</Button>);
 
   const button = await screen.getByTestId("button");
 
-  // ACT
+  expect(button).toHaveTextContent("Hello World");
+});
+
+test("click event", async () => {
+  const onClick = vitest.fn();
+  render(<Button onClick={onClick}>Hello World</Button>);
+
+  const button = await screen.getByTestId("button");
   await userEvent.click(button);
 
-  // ASSERT
-  expect(button).toHaveTextContent("Hello World");
+  expect(onClick).toHaveBeenCalledOnce();
 });
---
+++ src/tests/AppWithProviders.tsx
@@ -0,0 +1,23 @@
+import { FavoritesContextProvider } from "../contexts/FavoritesContext";
+import { RateContextProvider } from "../contexts/RateContext";
+import { LogsContextProvider } from "../contexts/LogsContext";
+import { HashRouter } from "react-router";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const AppWithProviders = ({ children }: { children: React.ReactNode }) => {
+  const queryClient = new QueryClient();
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <HashRouter>
+        <LogsContextProvider>
+          <FavoritesContextProvider>
+            <RateContextProvider>{children}</RateContextProvider>
+          </FavoritesContextProvider>
+        </LogsContextProvider>
+      </HashRouter>
+    </QueryClientProvider>
+  );
+};
+
+export default AppWithProviders;
