192b6db4dccdea273fdefd02d3da011a08ef6111
Florian Stancioiu <florinvictorstancioiu@gmail.com>
2026-06-30 16:31:25 +0300

    Group tests using describe function

--- src/components/UI/Button/Button.test.tsx
+++ src/components/UI/Button/Button.test.tsx
@@ -2,20 +2,22 @@ import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import Button from "./Button";
 
-test("component render", async () => {
-  render(<Button>Hello World</Button>);
+describe("<Button />", () => {
+  test("component render", async () => {
+    render(<Button>Hello World</Button>);
 
-  const button = await screen.getByTestId("button");
+    const button = await screen.getByTestId("button");
 
-  expect(button).toHaveTextContent("Hello World");
-});
+    expect(button).toHaveTextContent("Hello World");
+  });
 
-test("click event", async () => {
-  const onClick = vitest.fn();
-  render(<Button onClick={onClick}>Hello World</Button>);
+  test("click event", async () => {
+    const onClick = vitest.fn();
+    render(<Button onClick={onClick}>Hello World</Button>);
 
-  const button = await screen.getByTestId("button");
-  await userEvent.click(button);
+    const button = await screen.getByTestId("button");
+    await userEvent.click(button);
 
-  expect(onClick).toHaveBeenCalledOnce();
+    expect(onClick).toHaveBeenCalledOnce();
+  });
 });
