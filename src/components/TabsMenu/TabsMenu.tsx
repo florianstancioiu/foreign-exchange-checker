8c85487507e1bf7c4cb3cd6d960c33ce229f192a
Florian Stancioiu <florinvictorstancioiu@gmail.com>
2026-07-01 17:02:04 +0300

    Extracted tabs array into separate file

--- src/components/TabsMenu/TabsMenu.tsx
+++ src/components/TabsMenu/TabsMenu.tsx
@@ -3,43 +3,18 @@ import Dropdown from "../UI/Dropdown/Dropdown";
 import Tabs from "../UI/Tabs/Tabs";
 import { useFavoritesContext } from "../../contexts/FavoritesContext";
 import { useLogsContext } from "../../contexts/LogsContext";
+import tabs from "../../helpers/tabs";
 
 export type TabsMenuProps = {
   variant: "history" | "compare" | "favorites" | "log";
   children: ReactNode;
 };
 
-const dropdownValues = [
-  {
-    id: 1,
-    isActive: true,
-    url: "/",
-    title: "history",
-  },
-  {
-    id: 2,
-    url: "/compare",
-    title: "compare",
-  },
-  {
-    id: 3,
-    url: "/favorites",
-    title: "favorites",
-    suffix: 10,
-  },
-  {
-    id: 4,
-    url: "/log",
-    title: "log",
-    suffix: 8,
-  },
-];
-
 const TabsMenu = ({ variant, children }: TabsMenuProps) => {
   const { favorites } = useFavoritesContext();
   const { logs } = useLogsContext();
 
-  const actualDropdownValues = dropdownValues.map((val) => {
+  const actualTabs = tabs.map((val) => {
     const numberOfFavorites = favorites !== undefined ? favorites.length : 0;
     const numberOfLogs = logs !== undefined ? logs.length : 0;
 
@@ -57,8 +32,8 @@ const TabsMenu = ({ variant, children }: TabsMenuProps) => {
 
   return (
     <div className="px-4 md:px-6 xl:px-8">
-      <Dropdown values={actualDropdownValues} className="md:hidden" />
-      <Tabs values={actualDropdownValues} className="hidden md:block" />
+      <Dropdown values={actualTabs} className="md:hidden" />
+      <Tabs values={actualTabs} className="hidden md:block" />
       <div className="mt-4">{children}</div>
     </div>
   );
---
+++ src/helpers/tabs.ts
@@ -0,0 +1,25 @@
+export default [
+  {
+    id: 1,
+    isActive: true,
+    url: "/",
+    title: "history",
+  },
+  {
+    id: 2,
+    url: "/compare",
+    title: "compare",
+  },
+  {
+    id: 3,
+    url: "/favorites",
+    title: "favorites",
+    suffix: 10,
+  },
+  {
+    id: 4,
+    url: "/log",
+    title: "log",
+    suffix: 8,
+  },
+];
