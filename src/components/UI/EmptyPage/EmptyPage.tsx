83db1274532a687e6cf5380545e06610317d33fb
Florian Stancioiu <florinvictorstancioiu@gmail.com>
2026-07-01 17:21:20 +0300

    Wrote a test for EmptyPage component

--- src/components/UI/EmptyPage/EmptyPage.tsx
+++ src/components/UI/EmptyPage/EmptyPage.tsx
@@ -8,10 +8,16 @@ export type EmptyPageProps = {
 const EmptyPage = ({ title, content }: EmptyPageProps) => {
   return (
     <div className="py-10 px-0 flex flex-col justify-center items-center gap-y-4">
-      <p className="font-normal text-neutral-100 leading-[120%] tracking-[-0.5px] text-xl">
+      <p
+        className="font-normal text-neutral-100 leading-[120%] tracking-[-0.5px] text-xl"
+        data-testid="empty_page_title"
+      >
         {title}
       </p>
-      <div className="text-sm text-neutral-200 font-normal leading-[120%] tracking-[1px] text-center">
+      <div
+        className="text-sm text-neutral-200 font-normal leading-[120%] tracking-[1px] text-center"
+        data-testid="empty_page_content"
+      >
         {content}
       </div>
     </div>
