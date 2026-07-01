9dab8f7bac324685b9da900fc82f64f2ad8a1b4c
Florian Stancioiu <florinvictorstancioiu@gmail.com>
2026-07-01 18:26:11 +0300

    Wrote a test for Tabs component

--- src/components/UI/Tabs/Tabs.tsx
+++ src/components/UI/Tabs/Tabs.tsx
@@ -16,12 +16,13 @@ export type TabsProps = {
 
 const Tabs = ({ values, className }: TabsProps) => {
   return (
-    <div className={twMerge("pb-5", className)}>
+    <div className={twMerge("pb-5", className)} data-testid="tabs">
       {values.map((val) => (
         <LinkWithQuery
           key={val.id}
           className={`${val.isActive ? "border-lime-500" : "border-neutral-600"} uppercase text-base border-b-2 py-2.5 px-4 focus-visible:outline-lime-500 focus-visible:outline-2 focus-visible:outline-offset-3 focus-visible:rounded-lg`}
           to={val.url}
+          data-testid="tabs_link"
         >
           <span className="">{val.title}</span>
           {val.suffix !== undefined ? (
