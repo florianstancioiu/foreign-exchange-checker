0cbf26a027db365472800d821aef41a680a50a02
Florian Stancioiu <florinvictorstancioiu@gmail.com>
2026-07-03 10:24:36 +0300

    Worked on the Favorite page

--- src/pages/Favorites/FavoritesHeaderContent.tsx
+++ src/pages/Favorites/FavoritesHeaderContent.tsx
@@ -6,10 +6,10 @@ const FavoritesHeaderContent = ({
   numberOfFavorites,
 }: FavoritesHeaderContent) => (
   <div className="flex justify-between items-start mb-4 md:mb-5">
-    <p className="uppercase text-base font-medium leading-[120%] tracking-[1px] text-neutral-50">
+    <p className="uppercase text-base font-medium leading-[120%] tracking-[1px] text-neutral-50 light:text-neutral-900">
       Pinned pairs
     </p>
-    <p className="uppercase text-sm font-normal leading-[120%] tracking-[1px] text-neutral-200 mb-2.5">
+    <p className="uppercase text-sm font-normal leading-[120%] tracking-[1px] text-neutral-200 mb-2.5 light:text-neutral-700">
       {numberOfFavorites} favorites
     </p>
   </div>
