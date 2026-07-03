5a1800aff944b1ae2c0e735bc42b033bdcde5db1
Florian Stancioiu <florinvictorstancioiu@gmail.com>
2026-07-03 10:31:52 +0300

    Worked on the light mode for Log page

--- src/pages/Log/LogHeaderContent.tsx
+++ src/pages/Log/LogHeaderContent.tsx
@@ -11,18 +11,21 @@ const LogHeaderContent = ({ logged }: LogHeaderContent) => {
   return (
     <div className="flex justify-between items-end md:items-center">
       <div className="flex flex-col justify-between items-start gap-y-2.5">
-        <p className="uppercase text-base font-medium leading-[120%] tracking-[1px] text-neutral-50">
+        <p className="uppercase text-base font-medium leading-[120%] tracking-[1px] text-neutral-50 light:text-neutral-900">
           Conversion<span className="hidden sm:inline"> Log</span>
         </p>
-        <p className="uppercase text-sm font-normal leading-[120%] tracking-[1px] text-neutral-200 md:hidden">
+        <p className="uppercase text-sm font-normal leading-[120%] tracking-[1px] text-neutral-200 md:hidden light:text-neutral-700">
           {logged} Logged
         </p>
       </div>
       <div className="md:flex md:items-center md:gap-x-4">
-        <p className="uppercase text-sm font-normal leading-[120%] tracking-[1px] text-neutral-200 hidden md:block">
+        <p className="uppercase text-sm font-normal leading-[120%] tracking-[1px] text-neutral-200 hidden md:block light:text-neutral-700">
           {logged} Logged
         </p>
-        <Button onClick={() => clearAll()} className="text-neutral-200">
+        <Button
+          onClick={() => clearAll()}
+          className="text-neutral-200 light:text-neutral-100"
+        >
           Clear All
         </Button>
       </div>
