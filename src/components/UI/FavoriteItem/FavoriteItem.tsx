d2655287eb1d4f6d40a5a28115b9b53652b89cd2
Florian Stancioiu <florinvictorstancioiu@gmail.com>
2026-07-03 10:24:36 +0300

    Worked on the Favorite page

--- src/components/UI/FavoriteItem/FavoriteItem.tsx
+++ src/components/UI/FavoriteItem/FavoriteItem.tsx
@@ -13,22 +13,22 @@ const FavoriteItem = ({ firstCurrency, secondCurrency }: FavoriteItemProps) => {
   const { loadCurrencies } = useRateContext();
 
   return (
-    <section className="border border-neutral-500 rounded-[10px] bg-neutral-600 p-3 flex gap-x-2.5 justify-between items-center md:px-4 hover:border-neutral-300">
+    <section className="border border-neutral-500 rounded-[10px] bg-neutral-600 light:bg-blue-100 light:border-blue-200 p-3 flex gap-x-2.5 justify-between items-center md:px-4 hover:border-neutral-300">
       <button
         type="button"
         onClick={() => loadCurrencies(firstCurrency, secondCurrency)}
         data-testid="favorite_item_load_currencies_button"
-        className="flex items-center gap-2.5 text-sm font-normal leading-[120%] tracking-[1px] text-neutral-50 uppercase cursor-pointer"
+        className="flex items-center gap-2.5 text-sm font-normal leading-[120%] tracking-[1px] text-neutral-50 uppercase cursor-pointer light:text-neutral-900"
       >
         <span>{firstCurrency}</span>
-        <ArrowRightSvg />
+        <ArrowRightSvg className="light:text-neutral-900" />
         <span>{secondCurrency}</span>
       </button>
       <div className="flex justify-between items-center gap-x-2.5 md:gap-x-5">
         <button
           type="button"
           onClick={() => toggleFavorite(firstCurrency, secondCurrency)}
-          className="size-8 border border-lime-500 text-lime-500 rounded-[10px] grid place-content-center cursor-pointer focus-visible:outline-lime-500 focus-visible:outline-2 focus-visible:outline-offset-3 focus-visible:rounded-lg hover:bg-neutral-500"
+          className="size-8 border border-lime-500 text-lime-500 light:bg-neutral-900 light:border-neutral-900 rounded-[10px] grid place-content-center cursor-pointer focus-visible:outline-lime-500 focus-visible:outline-2 focus-visible:outline-offset-3 focus-visible:rounded-lg hover:bg-neutral-500"
           data-testid="favorite_item_toggle_favorite_button"
         >
           <StarFilledSvg />
