305a051a29a0231856d3d9dd57ebd8cace903647
Florian Stancioiu <florinvictorstancioiu@gmail.com>
2026-07-07 12:33:19 +0300

    Added tests for RateConverter component

---
+++ src/components/RateConverter/RateConverter.test.tsx
@@ -0,0 +1,57 @@
+import { render, screen } from "@testing-library/react";
+import RateConverter from "./RateConverter";
+import AppWithProviders from "../../tests/AppWithProviders";
+import { mockContexts } from "../../tests/mockContexts";
+import { mockRatesQuery } from "../../tests/queries/rates";
+import { mockCurrenciesQuery } from "../../tests/queries/currencies";
+
+describe("<RateConverter />", () => {
+  beforeEach(async () => {
+    mockContexts();
+    mockRatesQuery();
+    mockCurrenciesQuery();
+  });
+
+  test("component render", async () => {
+    const setCurrency = vi.fn();
+    render(
+      <AppWithProviders>
+        <RateConverter
+          title="Send"
+          value={1000}
+          currency="RON"
+          setCurrency={setCurrency}
+        />
+      </AppWithProviders>,
+    );
+
+    const title = screen.getByTestId("rate_converter_title");
+    const isReceive = screen.queryByTestId("rate_converter_is_receive");
+    const isSend = screen.queryByTestId("rate_converter_is_send");
+
+    expect(title).toHaveTextContent("Send");
+    expect(isReceive).not.toBeInTheDocument();
+    expect(isSend).toBeInTheDocument();
+  });
+
+  test("is receive state", async () => {
+    const setCurrency = vi.fn();
+    render(
+      <AppWithProviders>
+        <RateConverter
+          isReceive={true}
+          title="Receive"
+          value={1000}
+          currency="RON"
+          setCurrency={setCurrency}
+        />
+      </AppWithProviders>,
+    );
+
+    const isReceive = screen.queryByTestId("rate_converter_is_receive");
+    const isSend = screen.queryByTestId("rate_converter_is_send");
+
+    expect(isReceive).toBeInTheDocument();
+    expect(isSend).not.toBeInTheDocument();
+  });
+});
--- src/components/RateConverter/RateConverter.tsx
+++ src/components/RateConverter/RateConverter.tsx
@@ -29,13 +29,17 @@ const RateConverter = ({
 
   return (
     <div className="p-4 rounded-2xl border border-neutral-500 bg-neutral-600 w-full md:p-5 light:bg-blue-200 light:border-neutral-100">
-      <p className="uppercase text-sm text-neutral-100 mb-5 leading-[120%] tracking-[1px] light:text-neutral-900">
+      <p
+        className="uppercase text-sm text-neutral-100 mb-5 leading-[120%] tracking-[1px] light:text-neutral-900"
+        data-testid="rate_converter_title"
+      >
         {title}
       </p>
       <div className="flex justify-between items-center">
         {isReceive ? (
           <p
             className={`${isReceive ? "text-lime-500 light:text-lime-600" : ""} text-3xl font-semibold leading-[100%] tracking-[-0.5px]`}
+            data-testid="rate_converter_is_receive"
           >
             {typeof value === "number" && isFinite(value)
               ? toFixed(value, 2)
@@ -50,6 +54,7 @@ const RateConverter = ({
             value={value}
             onChange={onChangeSendValueHandler}
             className="text-3xl font-semibold leading-[100%] tracking-[-0.5px] max-w-30 sm:max-w-40 focus-visible:outline-lime-500 focus-visible:outline-2 focus-visible:outline-offset-3 focus-visible:rounded-lg light:focus-visible:outline-blue-500"
+            data-testid="rate_converter_is_send"
           />
         )}
         <CurrencyPicker activeISO={currency} setActiveIso={setCurrency} />
